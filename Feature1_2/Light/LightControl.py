686df6741159daa18abd45ba2da4bf3bc840dfa7
DeformiaR <113496368+DeformiaR@users.noreply.github.com>
2025-06-12 19:38:25 +0700

    Update Game logic to have achievement
    achievement for each action (light , watering plant , pet plant , Info looking)

--- Feature1_2/Light/LightControl.py
+++ Feature1_2/Light/LightControl.py
@@ -24,7 +24,7 @@ app.add_middleware(
 )
 
 # Replace with your actual bulb IP address
-bulb = wizlight("192.168.1.125")
+bulb = wizlight("192.168.1.153")
 
 class ColorCommand(BaseModel):
     r: Optional[int] = None
--- Feature_1/Ras_Pi/Bulb.py
+++ Feature_1/Ras_Pi/Bulb.py
@@ -6,7 +6,7 @@ from pywizlight import wizlight, PilotBuilder
 nest_asyncio.apply()
 
 # Your bulb IP
-bulb = wizlight("192.168.1.125")
+bulb = wizlight("192.168.1.153")
 
 async def main():
     print("🔌 Turning bulb ON (white)...")
